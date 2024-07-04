2c98707c9f0c52618fdbe712dbb7530d3c9f3a4e
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-07-04 09:09:30 +0000

    task_1/

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,4 +9,12 @@ interface Teacher {
 
 interface Directors extends Teacher {
   numberOfReports: number;
-}  
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
